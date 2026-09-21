aa60650820b3ec2383fafaee3c15156e8406e61f
KingShadow1332 <morice3474@gmail.com>
2026-09-21 23:01:00 +0200

    A.R.I Update: app/sw.js

--- app/sw.js
+++ app/sw.js
@@ -1,7 +1,7 @@
 // Offline-Cache: die App startet auch ohne Internet (KI-Aufrufe brauchen natuerlich Netz)
-const CACHE = 'ari-app-v1';
-const FILES = ['./', 'index.html', 'app.js', 'manifest.webmanifest', 'icon.svg'];
-self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())); });
+const CACHE = 'ari-app-v3';
+const FILES = ['./', 'index.html', 'app.js', 'manifest.webmanifest', 'icon.svg', 'icon-192.png', 'icon-512.png', 'icon-maskable-512.png', 'apple-touch-icon.png', 'favicon-32.png'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => Promise.all(FILES.map(f => c.add(f).catch(() => {})))).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener('fetch', e => {
   const u = new URL(e.request.url);
