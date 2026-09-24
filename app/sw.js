b79e621f2423596bef1b848adca3d673a7d15969
KingShadow1332 <morice3474@gmail.com>
2026-09-24 22:09:36 +0200

    A.R.I Update: app/sw.js

--- app/sw.js
+++ app/sw.js
@@ -1,5 +1,5 @@
 // Offline-Cache: die App startet auch ohne Internet (KI-Aufrufe brauchen natuerlich Netz)
-const CACHE = 'ari-app-v5';
+const CACHE = 'ari-app-v6';
 const FILES = ['./', 'index.html', 'app.js', 'jsqr.js', 'manifest.webmanifest', 'icon.svg', 'icon-192.png', 'icon-512.png', 'icon-maskable-512.png', 'apple-touch-icon.png', 'favicon-32.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => Promise.all(FILES.map(f => c.add(f).catch(() => {})))).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
